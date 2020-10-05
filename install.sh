4c25b2b3b49797b398ce547f16c14bf2425c0542
adamsyah <fadillahadam11@gmail.com>
2020-10-05 21:32:18 +0700

    Add jupyter in install.sh file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 conda activate py37
+conda install jupyter -y
 conda install -c intel numpy scipy scikit-learn -y
 conda install -c numba numba -y
 conda install -c conda-forge jupyterlab matplotlib plotly nodejs pymap3d pandas sympy -y
